c6086fae3b8bbb43afd9931dfd0f0648a0ee617f
RaulVM <erudito_01@hotmail.com>
2025-07-15 17:00:12 -0600

    Creacion de portafolio

---
+++ src/content_option.js
@@ -0,0 +1,171 @@
+const logotext = "RAÚL";
+const meta = {
+    title: "Raúl Villarreal Mora",
+    description: "Mi nombre es Raúl soy Ing. en sistemas computacionales _ desarrollador full Stack",
+};
+
+const introdata = {
+    title: "Soy Raúl Villarreal Mora",
+    animated: {
+        first: "Desarrollador autodidacta",
+        second: "Enfocado en la mejora continua",
+        third: "Buscador de la innovación",
+    },
+    description: "Ingeniero en Sistemas Computacionales con más de 4 años de experiencia en desarrollo de software. Apasionado por la tecnología, con habilidades autodidactas y orientado a la mejora continua. Experiencia en una amplia variedad de tecnologías y en la migración de aplicaciones a la nube. Busco constantemente nuevos retos para expandir mis habilidades.",
+    your_img_url: "/perfil.jpg",
+};
+
+const dataabout = {
+    title: "Descripcion",
+    aboutme: "Ingeniero en Sistemas Computacionales con más de 4 años de experiencia en desarrollo de software. Apasionado por la tecnología, con habilidades autodidactas y orientado a la mejora continua. Experiencia en una amplia variedad de tecnologías y en la migración de aplicaciones a la nube. Busco constantemente nuevos retos para expandir mis habilidades.",
+};
+const worktimeline = [{
+        jobtitle: "Fullstack developer",
+        where: "Infinita Consulting",
+        date: "2024",
+        end: "2025",
+    },
+    {
+        jobtitle: "Fullstack developer",
+        where: "Abisoft",
+        date: "2022",
+        end: "2024",
+    },
+    {
+        jobtitle: "Junior developer",
+        where: "Accesorios Desa",
+        date: "2021",
+        end: "2022",
+    },
+    {
+        jobtitle: "Junior developer",
+        where: "Software factory Itszas",
+        date: "2020",
+        end: "2021",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "raulvillarrealmora.dev@gmail.com",
+    YOUR_FONE: "(332)159-3042",
+    description: "Construyo aplicaciones web funcionales y eficientes usando tecnologías como JavaScript, Python y C#. Aquí muestro algunos de mis proyectos más destacados.", 
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Lemorax99",
+    linkedin: "https://www.linkedin.com/in/raul-villarreal-mora-612606199/"
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
